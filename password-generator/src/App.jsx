9f3099776cddd6954c858c273e592698c3105b0e
Wa Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2023-07-17 12:08:27 -0400

    Add all the UI Components of the app

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,4 +1,5 @@
 import copy_icon from "./assets/icon-copy.svg";
+import arrow_right from "./assets/icon-arrow-right.svg";
 
 export default function App() {
   return (
@@ -16,7 +17,7 @@ export default function App() {
       </div>
 
       {/* BODY */}
-      <div className="text-semi-gray bg-semi-black h-3/4 w-full border p-5">
+      <div className="text-semi-gray bg-semi-black w-full border p-5">
         {/* CHARACTER LENGTH CONTAINER */}
         <div className="character_length_container">
           <div className="flex flex-row justify-between items-center my-3">
@@ -83,6 +84,24 @@ export default function App() {
               </label>
             </div>
           </div>
+
+          {/* DIPLAY PASSWORD STRENGTH STATUS */}
+          <div className="flex flex-row justify-between bg-more-black p-5">
+            <h1 className="text-semi-gray font-semibold uppercase">Strength</h1>
+            <div className="text-semi-white font-semibold">MEDIUM</div>
+            <div className="strength_status_container flex flex-row justify-center w-1/3">
+              <div className="strength_status_child w-2 mx-1 border h-full bg-more-green"></div>
+              <div className="strength_status_child w-2 mx-1 border h-full bg-more-green"></div>
+              <div className="strength_status_child w-2 mx-1 border h-full bg-more-green"></div>
+              <div className="strength_status_child w-2 mx-1 border h-full bg-more-green"></div>
+            </div>
+          </div>
+
+          {/* GENERATE PASSWORD BUTTON */}
+          <button className=" bg-more-green my-10 p-5 w-full flex flex-row justify-center items-center">
+            <span className="text-more-black font-bold">GENERATE</span>
+            <img src={arrow_right} alt="arrow_right" className="mx-4" />
+          </button>
         </div>
       </div>
     </div>
