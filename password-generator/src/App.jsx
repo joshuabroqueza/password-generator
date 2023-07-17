c87f1923547240ce608d084f313eb23c1104b528
Wa Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2023-07-17 11:25:42 -0400

    Update the padding of the container

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -2,13 +2,13 @@ import copy_icon from "./assets/icon-copy.svg";
 
 export default function App() {
   return (
-    <div className="bg-more-black h-screen flex flex-col justify-center items-center font-jetbrains">
+    <div className="bg-more-black h-screen w-full px-5 flex flex-col justify-center items-center font-jetbrains">
       {/* HEADER */}
       <div>
         <h1 className="text-semi-gray text-xl">Password Generator</h1>
       </div>
       {/* PASSWORD VALUE */}
-      <div className="text-jetbrains password_value flex justify-between flex-row border w-96 p-5 bg-semi-black m-5">
+      <div className="text-jetbrains password_value flex justify-between flex-row border w-full p-5 bg-semi-black m-5">
         <div className="text-semi-white font-semibold">TEXT CONTENT HERE</div>
         <button>
           <img src={copy_icon} alt="copy icon" />
@@ -16,7 +16,53 @@ export default function App() {
       </div>
 
       {/* BODY */}
-      <div className="text-semi-gray bg-semi-black  h-3/4 w-96 border"></div>
+      <div className="text-semi-gray bg-semi-black h-3/4 w-full border p-5">
+        {/* CHARACTER LENGTH CONTAINER */}
+        <div className="character_length_container">
+          <div className="flex flex-row justify-between items-center my-3">
+            <h1 className="font-semibold text-semi-white">Character Length</h1>
+            <div className="character_length_value text-2xl text-more-green">
+              10
+            </div>
+          </div>
+          <div className="input_bar_container">
+            {/* INPUT BAR ELEMENT WITH A MINIMUM 1 AND MAX 20 VALUE */}
+
+            <input
+              min="1"
+              max="20"
+              type="range"
+              className="in-range:border-more-green w-full in-range:bg-more-green in-range:text-more-green"
+            />
+          </div>
+
+          {/* INCLUDE IN THE PASSWORD CONTAINER */}
+          <div>
+            <div className="p_include_child py-5 my-3">
+              <input
+                type="checkbox"
+                name="uppercase"
+                id="uppercase"
+                className="mr-10 active:bg-more-green"
+              />
+              <label htmlFor="uppercase" className="text-semi-white">
+                Include Uppercase Letters
+              </label>
+            </div>
+            <div className="p_include_child py-5 my-3">
+              <input
+                type="checkbox"
+                name="lowercase"
+                id="lowercase"
+                className="mr-10 active:bg-more-green"
+              />
+              <label htmlFor="lowercase" className="text-semi-white">
+                Include Lowercase Letters
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
