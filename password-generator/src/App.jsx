d420282883c269283c07279dcca1bb0a76023256
Wa Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2023-07-17 10:27:24 -0400

    Importing colors

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="font-jetbrains text-3xl font-bold underline">
+    <div className="font-jetbrains text-3xl bg-semi-red font-bold underline">
       Hello world!
     </div>
   );
--- password-generator/tailwind.config.js
+++ password-generator/tailwind.config.js
@@ -6,6 +6,17 @@ export default {
       fontFamily: {
         jetbrains: ["JetBrains Mono", "monospace"],
       },
+      colors: {
+        "regal-blue": "#243c5a",
+        "semi-black": "#24232C",
+        "semi-red": "#F64A4A",
+        "semin-gray": "#817D92  ",
+        "semi-white": "#E6E5EA",
+        "more-black": "#18171F",
+        "more-green": "#A4FFAF",
+        "semi-orange": "#FB7C58",
+        "semi-yellow": "#F9F871",
+      },
     },
   },
   plugins: [],
