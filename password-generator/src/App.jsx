be7eaee9ab2642cc919161a1318a7f582ffd250b
Wa Broqueza <47325805+joshuabroqueza@users.noreply.github.com>
2023-07-17 10:22:00 -0400

    Importing Jetbrains

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="font-jetbrains text-3xl font-bold underline">
+      Hello world!
+    </div>
+  );
 }
